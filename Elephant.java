c12ae5cf7bfb24721f59fb99076fcdcc68cbaa1e
Blank <guoshaoxuan1@gmail.com>
2025-05-05 20:15:44 -0400

    i

--- Elephant.java
+++ Elephant.java
@@ -9,8 +9,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSounds = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
     
-    
+    /**
+     * Constructor - The code that gets run one time when object is created
+     */
+    public Elephant()
+    {
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
